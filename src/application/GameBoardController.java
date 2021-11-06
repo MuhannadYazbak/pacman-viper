f7c8d7c59fe4d7cf9301fef80619431c03a7d6d4
MuhannadYazbak <yazbakm@gmail.com>
2021-11-06 10:49:02 +0200

    first commit

---
+++ src/application/GameBoardController.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class GameBoardController {
+	@FXML
+	Button backBtn;
+	@FXML
+	private void backToMain(ActionEvent event) throws IOException {
+		AnchorPane pane = FXMLLoader.load(getClass().getResource("Main.fxml"));
+		Scene scene = new Scene(pane);
+		Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+		stage.setScene(scene);
+		stage.setTitle("Main Screen");
+		stage.show();
+	}
+
+}
---
+++ src/application/MainController.java
@@ -0,0 +1,47 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+public class MainController {
+	
+	@FXML
+	Button startGameBtn, lastGamesBtn, questionsBtn;
+	@FXML
+	private void LastGames(ActionEvent event) throws IOException {
+		AnchorPane pane = FXMLLoader.load(getClass().getResource("LastGames.fxml"));
+		Scene scene = new Scene(pane);
+		Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+		stage.setScene(scene);
+		stage.setTitle("Last Games Screen");
+		stage.show();
+	}
+	@FXML
+	private void startGame(ActionEvent event) throws IOException {
+		AnchorPane pane = FXMLLoader.load(getClass().getResource("startGame.fxml"));
+		Scene scene = new Scene(pane);
+		Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+		stage.setScene(scene);
+		stage.setTitle("Start Game Screen");
+		stage.show();
+	}
+	@FXML
+	private void questions(ActionEvent event) throws IOException {
+		AnchorPane pane = FXMLLoader.load(getClass().getResource("questions.fxml"));
+		Scene scene = new Scene(pane);
+		Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+		stage.setScene(scene);
+		stage.setTitle("Questions Screen");
+		stage.show();
+	}
+
+}
